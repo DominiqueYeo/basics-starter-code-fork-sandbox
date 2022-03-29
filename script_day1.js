afcb00964b2d8cda498b422b84b2e385bd44cce8
DominiqueYeo <86916639+DominiqueYeo@users.noreply.github.com>
2022-03-29 20:02:30 +0800

    updated script_day1 with comments

--- script_day1.js
+++ script_day1.js
@@ -11,19 +11,26 @@
 
 ////////////    Errors //////////////////////////////
 // //Syntax error
-// var main = function (input) {
+// var main = function () {
 //   var myOutputValue = "hellogoodbye";
-//   retur myOutputValue;
+//   return myOutputValue;
 // };
 
 // // //Logical error
 // var addTwoNumbers = function (num1, num2) {
+//   console.log("first number is : ", num1, "and the second is : ", num2);
 //   return Number(num1) - Number(num2);
 // };
 
 // var main = function (input) {
 //   var firstNumber = input;
 //   var secondNumber = 5;
+//   console.log(
+//     "first number is : ",
+//     firstNumber,
+//     "and the second is : ",
+//     secondNumber
+//   );
 //   return addTwoNumbers(firstNumber, secondNumber);
 // };
 
@@ -39,73 +46,106 @@
 /////////////////////////  IF /////////////////////
 
 // var main = function (input) {
-//   var handsomeMeter = 'So-So'
-//   if (input == 'jiahao') {
-//     handsomeMeter = 'HANDSOME'
+//   var handsomeMeter = "So-So";
+//   if (input == "jiahao") {
+//     handsomeMeter = "HANDSOME";
 //   }
 
-//   return handsomeMeter
-// }
+//   return handsomeMeter;
+// };
 
-// // code still runs after if
-// // var main = function (input) {
-// //   var handsomeMeter = 'So-So'
-// //   if (input == 'jiahao') {
-// //     handsomeMeter = 'HANDSOME'
-// //   }
-// //   handsomeMeter = 'Erm okay la '
-// //   return handsomeMeter
-// // }
+//code still runs after if
+// var main = function (input) {
+//   var handsomeMeter = "So-So";
+//   if (input == "jiahao") {
+//     handsomeMeter = "HANDSOME";
+//   }
+//   handsomeMeter = "Erm okay la ";
+//   return handsomeMeter;
+// };
 
 // // early return to terminate code execution
-// // var main = function (input) {
-// //   var handsomeMeter = 'So-So'
-// //   if (input == 'jiahao') {
-// //     return 'HANDSOME'
-// //   }
-// //   handsomeMeter = 'Erm okay la '
-// //   return handsomeMeter
-// // }
+// var main = function (input) {
+//   var handsomeMeter = "So-So";
+//   if (input == "jiahao") {
+//     return "HANDSOME";
+//   }
+//   handsomeMeter = "Erm okay la ";
+//   return handsomeMeter;
+// };
 
 ///////////////////////  Else If ////////////////////////
 
+// simple else example
 // var main = function (input) {
-//   var handsomeMeter = 'So-So'
-//   if (input == 'jiahao') {
-//     handsomeMeter = 'HANDSOME'
+//   var handsomeMeter;
+//   if (input == "jiahao") {
+//     handsomeMeter = "HANDSOME";
 //   } else {
-//     handsomeMeter = 'Only Jia Hao handsome'
+//     handsomeMeter = "Only Jia Hao handsome";
 //   }
 
-//   return handsomeMeter
-// }
+//   return handsomeMeter;
+// };
 
-// // var main = function (input) {
-// //   var handsomeMeter = 'So-So'
-// //   if (input == 'jiahao') {
-// //     handsomeMeter = 'HANDSOME'
-// //   } else if (input == 'dom') {
-// //     handsomeMeter = 'HANDDDDSOOMMMEEEEEEEEE'
-// //   }
+// simple else if example
+// var main = function (input) {
+//   var handsomeMeter = "So-So";
+//   if (input == "jiahao") {
+//     handsomeMeter = "HANDSOME";
+//   } else if (input == "dom") {
+//     handsomeMeter = "HANDDDDSOOMMMEEEEEEEEE";
+//   }
 
-// //   return handsomeMeter
-// // }
+//   return handsomeMeter;
+// };
 
-// // var main = function (input) {
-// //   var handsomeMeter = 'So-So'
-// //   if (input == 'jiahao') {
-// //     handsomeMeter = 'HANDSOME'
-// //   } else if (input == 'dom') {
-// //     handsomeMeter = 'HANDDDDSOOMMMEEEEEEEEE'
-// //   } else {
-// //     handsomeMeter = 'Dom is the most handsome'
-// //   }
+// chaining  if - else if - else
+// var main = function (input) {
+//   var handsomeMeter = "So-So";
+//   if (input == "jiahao") {
+//     handsomeMeter = "HANDSOME";
+//   } else if (input == "dom") {
+//     handsomeMeter = "HANDDDDSOOMMMEEEEEEEEE";
+//   } else {
+//     handsomeMeter = "Dom is the most handsome";
+//   }
 
-// //   return handsomeMeter
-// // }
+//   return handsomeMeter;
+// };
+
+// example with two ifs, showing that both will run if conditions are met
+// var main = function (input) {
+//   var handsomeMeter = "So-So";
+//   if (input == "jiahao") {
+//     handsomeMeter = "HANDSOME";
+//     console.log("check jh", handsomeMeter);
+//   }
+//   if (handsomeMeter) {
+//     handsomeMeter = "HANDDDDSOOMMMEEEEEEEEE";
+//     console.log("check input", handsomeMeter);
+//   }
+
+//   return handsomeMeter;
+// };
+
+// example showing that only the first block of code that meets the condition will run in an if-else block
+// var main = function (input) {
+//   var handsomeMeter = "So-So";
+//   if (input == "jiahao") {
+//     handsomeMeter = "HANDSOME";
+//     console.log("check jh", handsomeMeter);
+//   } else if (handsomeMeter) {
+//     handsomeMeter = "HANDDDDSOOMMMEEEEEEEEE";
+//     console.log("check input", handsomeMeter);
+//   }
+
+//   return handsomeMeter;
+// };
 
 ////////////////////////////////// Logical OR //////////////////
 
+// simple OR example
 // var main = function (input) {
 //   var handsomeMeter = "So-So";
 //   if (input == "jiahao" || input == "dom") {
@@ -115,37 +155,39 @@
 //   return handsomeMeter;
 // };
 
-// // var main = function (input) {
-// //   var handsomeMeter = "So-So";
-// //   if (input == "jiahao" && input == "dom") {
-// //     handsomeMeter = "HANDSOME";
-// //   }
+// simple AND example
+// var main = function (input) {
+//   var handsomeMeter = "So-So";
+//   console.log("input", input, "condition", input == "jiahao" && input == "dom");
+//   if (input == "jiahao" && input == "dom") {
+//     handsomeMeter = "HANDSOME";
+//   }
 
-// //   return handsomeMeter;
-// // };
+//   return handsomeMeter;
+// };
 
-// // var main = function (input) {
-// //   var handsome = 10
-// //   var height = 178
-// //   var wallet = 10
-// //   if ( height > 190  || wallet > 1000000 || handsome > 8 ) {
-// //     return "Marry him now";
-// //   }
-// //   if ( height > 190  && wallet > 1000000 && handsome > 8 ) {
-// //     return "Marry him now";
-// //   }
-// // };
+// var main = function (input) {
+//   var handsome = 10;
+//   var height = 178;
+//   var wallet = 10;
+//   // if (height > 190 || wallet > 1000000 || handsome > 8) {
+//   //   return "Marry him now";
+//   // }
+//   if (height > 190 && wallet > 1000000 && handsome > 8) {
+//     return "Marry him now";
+//   }
+//   return "NO";
+// };
 
-// // var main = function (input) {
-// //   var handsome = 10
-// //   var height = 178
-// //   var wallet = 10
-// //   if ( height > 190  || wallet > 1000000 || handsome > 8 ) {
-// //     return "Marry him now";
-// //   }
-// //   if ( height > 190  && wallet > 1000000 && handsome > 8 ) {
-// //     return "Marry him now";
-// //   }else if (input == 'dom') {
-// //     return 'Marry him now!'
-// //   }
-// // };
+// using logical operators in an if-else block
+var main = function (input) {
+  var handsome = 10;
+  var height = 178;
+  var wallet = 10;
+  if (height > 190 && wallet > 1000000 && handsome > 8) {
+    return "Marry him now";
+  } else if (input == "dom") {
+    return "Marry him now!";
+  }
+  return "NO";
+};
